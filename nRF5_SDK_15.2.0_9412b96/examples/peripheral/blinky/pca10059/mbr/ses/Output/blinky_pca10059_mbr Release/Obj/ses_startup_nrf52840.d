50e1fa5a3f5de635ec2be9d047689bc22f98336a
FloBoth <florian.bothereau@insa-lyon.fr>
2018-11-21 17:46:39 +0100

    first commit

---
+++ nRF5_SDK_15.2.0_9412b96/examples/peripheral/blinky/pca10059/mbr/ses/Output/blinky_pca10059_mbr Release/Obj/ses_startup_nrf52840.d
@@ -0,0 +1,2 @@
+Output/blinky_pca10059_mbr\ Release/Obj/ses_startup_nrf52840.o: \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/ses_startup_nrf52840.s
---
+++ nRF5_SDK_15.2.0_9412b96/examples/peripheral/blinky/pca10059/mbr/ses/Output/blinky_pca10059_mbr Release/Obj/system_nrf52840.d
@@ -0,0 +1,16 @@
+Output/blinky_pca10059_mbr\ Release/Obj/system_nrf52840.o: \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/system_nrf52840.c \
+ /usr/share/segger_embedded_studio_for_arm_4.10a/include/stdint.h \
+ /usr/share/segger_embedded_studio_for_arm_4.10a/include/stdbool.h \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/nrf.h \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/system_nrf52840.h \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/nrf52840_bitfields.h \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ /home/fbothereau/Documents/PILS/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/compiler_abstraction.h
